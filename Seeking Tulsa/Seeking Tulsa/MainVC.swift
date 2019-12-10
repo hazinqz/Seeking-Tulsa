8c6255f4795cd3be0194143885c1e4236a9f19a6
hazinqz <57641352+hazinqz@users.noreply.github.com>
2019-12-10 14:42:48 -0600

    Add files via upload

--- Seeking Tulsa/Seeking Tulsa/MainVC.swift
+++ Seeking Tulsa/Seeking Tulsa/MainVC.swift
@@ -12,6 +12,13 @@ import MapKit
 class MainVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBAction func mapTypeChanged(_ sender: Any) {
+     
+                  mapView.mapType = MKMapType.init(rawValue: UInt((sender as AnyObject).selectedSegmentIndex)) ?? .standard
+          
+          
+          }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,53 +31,14 @@ class MainVC: UIViewController {
                 let coordinateRegoin = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
                 mapView.setRegion(coordinateRegoin, animated: true)
 
-                centerMapOnLocation(location: initialLocation)
-
         }
         
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(showMap),
-                                               name: NSNotification.Name("ShowProfile"),
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(showSettings),
-                                               name: NSNotification.Name("ShowSettings"),
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(showSignIn),
-                                               name: NSNotification.Name("ShowSignIn"),
-                                               object: nil)
-    }
-    
-    @objc func showMap() {
-        performSegue(withIdentifier: "ShowMap", sender: nil)
-    }
-    
-    @objc func showSettings() {
-        performSegue(withIdentifier: "ShowSettings", sender: nil)
-    }
-    
-    @objc func showSignIn() {
-        performSegue(withIdentifier: "ShowSignIn", sender: nil)
-    }
-    
-    
-    @IBAction func onMoreTapped() {
-        print("TOGGLE SIDE MENU")
-        NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
-    }
-  
-    @IBAction func mapTypeChanged(_ sender: Any) {
-   
-                mapView.mapType = MKMapType.init(rawValue: UInt((sender as AnyObject).selectedSegmentIndex)) ?? .standard
-        
-        
-        }
+         centerMapOnLocation(location: initialLocation)
     
     
 
 }
 
-  
+}
     
 
